aaa9204d1caaa0fe31438fd66e660f2f533ef3bb
Aleksandr Chernishov <caa190993@gmail.com>
2020-10-26 13:02:57 +0200

    Подготовил файлы к дальнейшей работе

---
+++ JS/refs.js
@@ -0,0 +1,8 @@
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+const refs = {
+    checkbox: document.querySelector('#theme-switch-toggle'),
+    menuList: document.querySelector('ul.js-menu'),
+}
---
+++ src/index.js
@@ -0,0 +1,3 @@
+import * as styles from './styles.css';
+import * as refs from '../JS/refs'
+import * as onThemeChange from '../JS/onThemeChange'
